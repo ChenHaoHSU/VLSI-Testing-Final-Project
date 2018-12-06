190f8c35b9206bd845503d892dc1327dacf859bd
FuyuChuang <good570520@gmail.com>
2018-12-06 14:25:22 +0800

    fill podem todo, basic system structure

--- src/tdfatpg.cpp
+++ src/tdfatpg.cpp
@@ -19,6 +19,55 @@ void ATPG::transition_delay_fault_atpg(void) {
   int no_of_redundant_faults = 0;
   int no_of_calls = 0;
 
+  fptr fault_under_test = select_primary_fault();
+
+  /* generate test pattern */
+  while (fault_under_test != nullptr) {
+    switch (podem(fault_under_test, current_backtracks)) {
+      case TRUE:
+        /* form a vector */
+        vec.clear();
+        for (wptr w: cktin) {
+          vec.push_back(itoc(w->value));
+        }
+        /*by defect, we want only one pattern per fault */
+        /*run a fault simulation, drop ALL detected faults */
+        if (total_attempt_num == 1) {
+          fault_sim_a_vector(vec, current_detect_num);
+          total_detect_num += current_detect_num;
+        }
+        /* If we want mutiple petterns per fault, 
+         * NO fault simulation.  drop ONLY the fault under test */ 
+        else {
+          fault_under_test->detect = TRUE;
+          /* drop fault_under_test */
+          flist_undetect.remove(fault_under_test);
+        }
+        in_vector_no++;
+        break;
+
+	  case FALSE:
+        fault_under_test->detect = REDUNDANT;
+        no_of_redundant_faults++;
+        break;
+  
+	  case MAYBE:
+        no_of_aborted_faults++;
+        break;
+    }
+    fault_under_test->test_tried = true;
+    fault_under_test = nullptr;
+    for (fptr fptr_ele: flist_undetect) {
+      if (!fptr_ele->test_tried) {
+        fault_under_test = fptr_ele;
+        break;
+      }
+    }
+    total_no_of_backtracks += current_backtracks; // accumulate number of backtracks
+    no_of_calls++;
+  }
+
+
   display_undetect();
   fprintf(stdout,"\n");
   fprintf(stdout,"#number of aborted faults = %d\n",no_of_aborted_faults);
@@ -29,3 +78,43 @@ void ATPG::transition_delay_fault_atpg(void) {
   fprintf(stdout,"\n");
   fprintf(stdout,"#total number of backtracks = %d\n",total_no_of_backtracks);
 }
+
+/* select a primary fault for podem */
+ATPG::fptr ATPG::select_primary_fault()
+{
+  fptr fault_selected = flist_undetect.front();
+
+  return fault_selected;
+}      
+
+/* select a secondary fault for podem_x */
+ATPG::fptr ATPG::select_secondary_fault()
+{
+  fptr fault_selected = flist_undetect.front();
+
+  return fault_selected;
+}
+
+/* generate test vector 1, considering fault/LOS constraints */
+void ATPG::tdf_podem_v1(const fptr fault) 
+{
+
+}       
+
+/* generate test vector 2, injection/activation/propagation */
+void ATPG::tdf_podem_v2(const fptr fault) 
+{
+
+}       
+
+/* dynamic test compression by podem-x */
+void ATPG::tdf_podem_x()  
+{
+
+}
+
+/* static test compression */
+void ATPG::static_compression()  
+{
+
+}          
