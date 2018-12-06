63b5aa28c3a4c50669e7df2029f99b7500f31497
ChenHaoHSU <b03505028@ntu.edu.tw>
2018-12-06 14:41:59 +0800

    upd eval

--- eval.sh
+++ eval.sh
@@ -7,4 +7,16 @@ PAT_PATH=./tdf_patterns
 CKT_FILE=$CIR_PATH/c$1.ckt
 PAT_FILE=$PAT_PATH/c$1.pat
 
-$EVAL -tdfsim $PAT_FILE $CKT_FILE
+print_help () {
+    echo "[Help]                   "
+    echo "Usage:                   "
+    echo "         ./eval.sh <ckt#>"
+    echo "Example:                 "
+    echo "         ./eval.sh 17    "
+}
+
+if [ $# -eq 1 ]; then
+    $EVAL -tdfsim $PAT_FILE $CKT_FILE
+else
+    print_help
+fi
